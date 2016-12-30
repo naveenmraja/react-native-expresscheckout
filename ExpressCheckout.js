5875f55faacf487f22e5a44823a500383d5c8e11
Naveen <naveen.raja@juspay.in>
2016-12-30 19:56:55 +0530

    exports changes

--- ExpressCheckout.js
+++ ExpressCheckout.js
@@ -1,4 +1,28 @@
 'use strict';
 
 import { NativeModules } from 'react-native';
-module.exports = NativeModules.ExpressCheckoutScene
+
+export class ExpressCheckout {
+
+  static expressCheckoutScene(paymentOptions, endUrlCallback, errorCallback) {
+    NativeModules.ExpressCheckoutScene.startPayment(paymentOptions, endUrlCallback, errorCallback);
+  }
+
+  static mobileWebCheckoutScene(paymentOptions, endUrlCallback, errorCallback) {
+    NativeModules.MobileWebCheckoutScene.startPayment(paymentOptions, endUrlCallback, errorCallback);
+  }
+
+  static cardPaymentAPI(paymentOptions, endUrlCallback, errorCallback) {
+    NativeModules.CardPaymentAPI(paymentOptions, endUrlCallback, errorCallback);
+  }
+
+  static netBankingPaymentAPI(paymentOptions, endUrlCallback, errorCallback) {
+    NativeModules.NetBankingPaymentAPI(paymentOptions, endUrlCallback, errorCallback);
+  }
+
+  static walletPaymentAPI(paymentOptions, endUrlCallback, errorCallback) {
+    NativeModules.WalletPaymentAPI(paymentOptions, endUrlCallback, errorCallback);
+  }
+}
+
+export default ExpressCheckout;
