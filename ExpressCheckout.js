26b411b3c766eed9156571e7063f4ca332f3c8dd
Naveen <naveen.raja@juspay.in>
2016-12-30 19:34:28 +0530

    minor change

--- ExpressCheckout.js
+++ ExpressCheckout.js
@@ -1,5 +1,4 @@
 'use strict';
 
 import { NativeModules } from 'react-native';
-module.exports = {NativeModules.MobileWebCheckoutScene, NativeModules.CardPaymentAPI,
-  NativeModules.NetBankingPaymentAPI, NativeModules.WalletPaymentAPI, NativeModules.ExpressCheckoutScene}
+module.exports = NativeModules.ExpressCheckoutScene
